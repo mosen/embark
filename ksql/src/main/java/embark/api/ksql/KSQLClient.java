b21a4deaf673c619d5dacde940402fd3e7b12479
Mosen <mosen@users.noreply.github.com>
2019-12-04 22:12:10 +1100

    Remove local config Move kerberos notes to different document

--- ksql/src/main/java/embark/api/ksql/KSQLClient.java
+++ ksql/src/main/java/embark/api/ksql/KSQLClient.java
@@ -8,10 +8,10 @@ import io.micronaut.http.annotation.Post;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.http.annotation.Get;
 import io.reactivex.Single;
-import embark.api.ksql.dto.CommandStatusResultDto;
-import embark.api.ksql.dto.KSQLExecuteRequestDto;
-import embark.api.ksql.dto.KSQLInfoDto;
-import embark.api.ksql.dto.KSQLRestResultDto;
+//import embark.api.ksql.dto.CommandStatusResultDto;
+//import embark.api.ksql.dto.KSQLExecuteRequestDto;
+//import embark.api.ksql.dto.KSQLInfoDto;
+//import embark.api.ksql.dto.KSQLRestResultDto;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
@@ -19,18 +19,18 @@ import javax.validation.constraints.NotBlank;
 @Client("${kafka.ksql.url}")
 @Header(name="Accept", value="application/vnd.ksql.v1+json")
 public interface KSQLClient {
-
-    @Get("/info")
-    public Single<HttpResponse<KSQLInfoDto>> info();
-
-    @Post("/ksql")
-    public Single<HttpResponse<KSQLRestResultDto[]>> ksql(@Body @Valid KSQLExecuteRequestDto requestDto);
-
-    @Post("/query")
-    public Single<HttpResponse<KSQLRestResultDto[]>> query(@Body @Valid KSQLExecuteRequestDto requestDto);
-
-    @Get("/status/{commandId}")
-    public Single<HttpResponse<CommandStatusResultDto>> status(@NotBlank String commandId);
+//
+//    @Get("/info")
+//    public Single<HttpResponse<KSQLInfoDto>> info();
+//
+//    @Post("/ksql")
+//    public Single<HttpResponse<KSQLRestResultDto[]>> ksql(@Body @Valid KSQLExecuteRequestDto requestDto);
+//
+//    @Post("/query")
+//    public Single<HttpResponse<KSQLRestResultDto[]>> query(@Body @Valid KSQLExecuteRequestDto requestDto);
+//
+//    @Get("/status/{commandId}")
+//    public Single<HttpResponse<CommandStatusResultDto>> status(@NotBlank String commandId);
 
 //    @Post("/terminate")
 //    public Single<HttpStatus> terminateCluster();
